9913f615828044788347863d5f7730ed4dd793ce
Dennis Keßel <dennis.kessel@gmail.com>
2025-12-29 11:47:46 +0100

    Implement participant name input for Wichtel-Generator
    Added functionality for participant name input and display.

--- wichtel-generator.py
+++ wichtel-generator.py
@@ -1,13 +1,37 @@
 ### Wichtel-Generator
 
-### Mit diesem Generatorprojekt sollen 
+### Mit diesem Generatorprojekt soll eine Gruppe von Menschen eine Losung durchführen können, damit jeder
+### eine Person erhält, welche er beschenken darf.
 
 ### Namenseingabe
 
-### Speicherung der Namen 
+anzahl_teilnehmer = int(input("Wieviele Teilnehmer gibt es?"))
+int(anzahl_teilnehmer)
+print(f"In dieser Runde gibt es insgesamt {anzahl_teilnehmer} Wichtel.")
+teilnehmerliste = []
 
-### Ziehung der Namen 
+while anzahl_teilnehmer > 0:
+    teilnehmer = input("Bitte gib deinen Vornamen ein!")
+    teilnehmerliste.append(teilnehmer)
+    anzahl_teilnehmer = anzahl_teilnehmer - 1
+
+print(teilnehmerliste)
+
+
+### Speicherung der Namen
+
+### teilnehmerliste = []
+### teilnehmerliste.append(teilnehmer)
+### print(teilnehmerliste)
+
+### Ziehung der Namen
+
+
 
 ### Ausschluss der Ziehung der gleichen Namen (jeder nur 1x)
 
 ### Anzahl der Ziehungen
+
+### Entfernung des Namens aus dem Topf
+
+### Automatische Sendung des Namens per E-Mail
