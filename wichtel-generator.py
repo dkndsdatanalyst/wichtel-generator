5c975359b0c5be8df29883fc8d77dd34b8a4a7c1
Dennis Keßel <dennis.kessel@gmail.com>
2025-12-29 10:39:53 +0100

    Add files via upload

---
+++ wichtel-generator.py
@@ -0,0 +1,13 @@
+### Wichtel-Generator
+
+### Mit diesem Generatorprojekt sollen 
+
+### Namenseingabe
+
+### Speicherung der Namen 
+
+### Ziehung der Namen 
+
+### Ausschluss der Ziehung der gleichen Namen (jeder nur 1x)
+
+### Anzahl der Ziehungen
